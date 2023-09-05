74aeac87fbcff15ad412314b5ec024822608a942
Jane Kostevich <yana.kostevich18@gmail.com>
2023-09-05 23:59:08 +0300

    desktop done

--- app/public/wp-content/themes/rgb-test/page-homepage.php
+++ app/public/wp-content/themes/rgb-test/page-homepage.php
@@ -7,9 +7,11 @@ get_header();
         <div class="description">
             <article class="frontend-course">
                 <p><img src="../../../wp-content/themes/rgb-test/img/globe.svg" alt="Кирил Касатонов">
-                    <span>Вебінар</span></p>
-                <h2>Front-End з нуля</h2>
-                <h3>Легкий старт в IT професії. Дізнайтеся, які навички повинен мати фронтенд-розробник у 2022 році та як розпочати кар'єру в цій востребованій професії.</h3>
+                    <span>Вебинар</span>
+                </p>
+                <h2>Front-End</h2>
+                <h3><span>с нуля</span> легкий старт в IT профессии.</h3>
+                <h4>Узнайте какими <span> навыками должен обладать фронтенд разработчик в 2022 году </span> и как начать карьеру в востребованной профессии </h4>
             </article>
             <aside class="coaches-and-bonus">
                 <secstion class="coach">
@@ -17,8 +19,8 @@ get_header();
                         <img src="../../../wp-content/themes/rgb-test/img/coach.svg" alt="Кирил Касатонов">
                     </div>
                     <div class="coach-info">
-                        <h4>Кирил Касатонов</h4>
-                        <p>6 років комерційного досвіду з такими компаніями, як Mercedes-Benz та іншими крупними корпораціями.</p>
+                        <h5>Кирил <span> Касатонов</span></h5>
+                        <p>6 лет коммерческого опыта с такими компаниями как Mercedes-Benz и другими крупными корпорациями</p>
                     </div>
                 </secstion>
                 <section class="registration-bonus">
@@ -26,14 +28,16 @@ get_header();
                         <img src="../../../wp-content/themes/rgb-test/img/bonus.svg" alt="Іконка бонусу за реєстрацію">
                     </div>
                     <div class="bonus-info">
-                        <h5>Бонус за реєстрацію</h5>
-                        <p>Отримайте бонус за реєстрацію на вебінар. 6 років комерційного досвіду з такими компаніями, як Mercedes-Benz та іншими крупними корпораціями.</p>
+                        <h5>Бонус за регистрацию</h5>
+                        <p>PDF-файл "5 преимуществ профессии фронтенд разработчика" </p>
                     </div>
                 </section>
             </aside>
         </div>
         <div class="registration-form">
-
+            <div class="form">
+                <?php echo do_shortcode('[contact-form-7 id="3f5b791" title="Contact form 1"]') ?>
+            </div>
         </div>
     </section>
 
